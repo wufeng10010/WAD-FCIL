e99a49c5e3b9e74d5763e05967c2b99796a2777d
wufeng10010 <131955051+wufeng10010@users.noreply.github.com>
2025-11-01 19:33:13 +0800

    Update myNetwork.py

--- WAD-FCIL/myNetwork.py
+++ WAD-FCIL/myNetwork.py
@@ -29,6 +29,27 @@ class network(nn.Module):
     def predict(self, fea_input):
         return self.fc(fea_input)
 
+class LeNet(nn.Module):
+    def __init__(self, channel=3, hideen=768, num_classes=10):
+        super(LeNet, self).__init__()
+        act = nn.Sigmoid
+        self.body = nn.Sequential(
+            nn.Conv2d(channel, 12, kernel_size=5, padding=5 // 2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2),
+            act(),
+            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1),
+            act(),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(hideen, num_classes)
+        )
+
+    def forward(self, x):
+        out = self.body(x)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
 def weights_init(m):
